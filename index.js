48f8d93884f5167b20902b4a46a544c891c6b2b2
yves <yvesmomplais@hotmail.com>
2021-07-06 20:05:48 -0400

    commit tree bc1897ea0dff55d6ebc54b64c30cd98c5ddbb3a2

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+const random = Math.floor(Math.random() * 1) + 1
+const num3 = 12
+const num4 = 8
+const mod = 4
+const max = 20
